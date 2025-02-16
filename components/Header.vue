8d1cb108f3a1d704cf484a956d7d2e23fd788c45
Gehadazzam <gehadgamalazzam@gmail.com>
2025-02-16 18:44:39 +0200

    feat: init header

---
+++ components/Header.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header
+    class="bg-cranberry-400 text-white py-2 px-4 sm:opx-8 md:px-12 lg:px-24 flex justify-between items-center"
+  >
+    <NuxtLink to="/">
+      <img src="/public/images/logo.png" alt="Logo" class="h-12 w-12" />
+    </NuxtLink>
+    <nav>
+      <ul class="flex space-x-4">
+        <li>
+          <nuxt-link to="/">Home</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/about">About</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/contact">Contact</nuxt-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,13 @@
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
+  alias: {
+    "@": resolve(__dirname, "./src"),
+  },
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
   vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+    plugins: [tailwindcss()],
   },
 });
