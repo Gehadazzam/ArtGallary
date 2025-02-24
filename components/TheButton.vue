8003782650a8e93bd57f9381385f95ac12842744
Gehadazzam <gehadgamalazzam@gmail.com>
2025-02-25 00:52:12 +0200

    feat: add the button component

---
+++ components/TheButton.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <button
+    class="bg-cranberry-500 hover:bg-cranberry-600 transition-colors justify-center items-center flex gap-2 w-full text-white px-4 py-2 rounded-lg"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped></style>
