029fc0f9c077cc1cf86f232ea00a5dad5956e4e7
Gehadazzam <gehadgamalazzam@gmail.com>
2025-03-10 07:25:21 +0200

    feat: add RegistrationPopUp component and integrate it with UserLogin for registration flow

---
+++ components/RegisterationPopUp.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const emit = defineEmits(["close"]);
+const close = () => {
+  emit("close");
+};
+onMounted(() => {
+  console.log("mounted");
+});
+</script>
+
+<template>
+  <template>
+    <div
+      class="bg-background m-4 md:m-8 lg:mx-24 lg:mt-12 rounded-4xl p-6 relative"
+    >
+      <button @click="close" class="absolute top-2 right-2">
+        <NuxtImg src="/images/Vector.svg" alt="close Pop Up" />
+      </button>
+      <h2>
+        A link has been sent to verify your identity. Please check your email to
+        complete the registration process.
+      </h2>
+      <TheButton @click="close" class="mt-4"> Thanks!</TheButton>
+    </div>
+  </template>
+</template>
--- components/UserLogin.vue
+++ components/UserLogin.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
-import { ref } from "vue";
 import { useForm } from "@formwerk/core";
-import { loginSchema, userSchema } from "~/utils/userSchema";
-import apiClient from "../utils/apiClient";
 
 const authMode = ref("login");
+const openPopup = ref(false);
 
 const loginErrorMessage = ref("");
 const {
@@ -26,7 +24,6 @@ const {
 
 // Login form submission
 const submitLoginForm = handleLoginSubmit(async (data) => {
-  console.log(JSON.stringify(data.toObject()));
   try {
     const response = await apiClient.post("/auth/login", data);
     sessionStorage.setItem("accessToken", response.data.accessToken);
@@ -54,11 +51,11 @@ const submitLoginForm = handleLoginSubmit(async (data) => {
 
 // Register form submission
 const submitRegisterForm = handleRegisterSubmit(async (data) => {
-  console.log(JSON.stringify(data.toObject()));
+  console.log("data");
   try {
     const response = await apiClient.post("/auth/register", data);
-    console.log(response);
-    authMode.value = "login";
+    openPopup.value = true;
+    console.log(openPopup.value);
   } catch (error) {
     if (
       error.response &&
@@ -80,120 +77,102 @@ const toggleAuthMode = () => {
   registerErrorMessage.value = "";
 };
 
+const closePopUP = () => {
+  openPopup.value = false;
+};
 const forgetPassowrd = () => {
-  console.log("Forget Password");
+  window.location.href = "/forget_password";
 };
 </script>
 
 <template>
-  <div
-    class="flex h-[80vh] flex-col items-center justify-center p-8 rounded-lg"
-  >
-    <div class="w-full h-full bg-[url('~/public/images/mainbg.svg')] bg-center">
-      <!-- Logo -->
-      <NuxtLink to="/">
-        <NuxtImg src="/images/logo.png" alt="logo" class="w-40 mx-auto mb-6" />
-      </NuxtLink>
-      <div class="w-2/3 sm:w-1/2 mx-auto">
-        <!-- Tab Buttons -->
-        <div
-          class="w-full flex bg-background mb-6 rounded-full border border-text p-1"
-        >
-          <button
-            @click="authMode = 'register'"
-            :class="[
-              'flex-1 py-2 text-center rounded-full transition-colors',
-              authMode === 'register' ? 'bg-button text-white' : 'text-text',
-            ]"
-          >
-            Sign Up
-          </button>
-          <button
-            @click="authMode = 'login'"
-            :class="[
-              'flex-1 py-2 text-center rounded-full transition-colors',
-              authMode === 'login' ? 'bg-button text-white' : 'text-text',
-            ]"
-          >
-            Sign In
-          </button>
-        </div>
-
-        <!-- Login Form -->
-        <form
-          v-if="authMode === 'login'"
-          @submit.prevent="submitLoginForm"
-          class="w-full flex flex-col gap-4 relative"
-        >
-          <TextField name="identifier" label="Username or Email" required />
-          <TextField
-            name="password"
-            label="Password"
-            type="password"
-            required
-          />
-          <p
-            class="underline text-text text-right absolute top-18 right-0 cursor-pointer"
-            @click="forgetPassowrd"
-          >
-            Forget Password?
-          </p>
-          <p
-            v-show="loginErrorMessage"
-            class="mt-4 border border-cranberry-400 rounded p-2 bg-cranberry-100 text-center text-sm text-cranberry-400"
-          >
-            {{ loginErrorMessage }}
-          </p>
-          <TheButton :disabled="isLoginSubmitting" type="submit" class="w-full">
-            {{ isLoginSubmitting ? "Logging in..." : "Login" }}
-            <img
-              src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAy0lEQVR4nM3TUUqCURAF4J+MelFXILmKENTA1Si1CteQuQJRtxDtxUpsEb59MXTJ+Pv/bqAPnrfLmTn3cGamKE4N3GGJLfb4wBrDXGMDc7zhAV1coYMxXhN/WSfwhBe0a/gmnkOkiuxhE0UZl63kcJBJ4gBMS+8JVsV/4QvfIriJYKuKchil2uuYzjEOutgdk8F97Em56DbNOTeFNt7RryJnaQ9afzQHP6tTv8BjaRMjsEg9bMfP8Un1Jv4QGmCRGuIWduk2fts+C3wCojcuU/AEC20AAAAASUVORK5CYII="
-              alt="login-rounded-right"
-            />
-          </TheButton>
-        </form>
-
-        <!-- Register Form -->
-        <form
-          v-else
-          @submit.prevent="submitRegisterForm"
-          class="w-full flex flex-col gap-4"
-        >
-          <TextField name="email" label="Email" required />
-          <TextField name="username" label="Name" required />
-          <TextField
-            name="password"
-            label="Password"
-            type="password"
-            required
-          />
-          <TextField
-            name="confirmPassword"
-            label="Confirm Password"
-            type="password"
-            required
-          />
-          <p
-            v-show="registerErrorMessage"
-            class="mt-4 border border-cranberry-400 rounded p-2 bg-cranberry-100 text-center text-sm text-cranberry-400"
-          >
-            {{ registerErrorMessage }}
-          </p>
-          <TheButton
-            :disabled="isRegisterSubmitting"
-            type="submit"
-            class="w-full"
-          >
-            {{
-              isRegisterSubmitting ? "Creating account..." : "Create account"
-            }}
-            <img
-              src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAy0lEQVR4nM3TUUqCURAF4J+MelFXILmKENTA1Si1CteQuQJRtxDtxUpsEb59MXTJ+Pv/bqAPnrfLmTn3cGamKE4N3GGJLfb4wBrDXGMDc7zhAV1coYMxXhN/WSfwhBe0a/gmnkOkiuxhE0UZl63kcJBJ4gBMS+8JVsV/4QvfIriJYKuKchil2uuYzjEOutgdk8F97Em56DbNOTeFNt7RryJnaQ9afzQHP6tTv8BjaRMjsEg9bMfP8Un1Jv4QGmCRGuIWduk2fts+C3wCojcuU/AEC20AAAAASUVORK5CYII="
-              alt="login-rounded-right"
-            />
-          </TheButton>
-        </form>
-      </div>
+  <!-- Logo -->
+  <NuxtLink to="/">
+    <NuxtImg src="/images/logo.png" alt="logo" class="w-40 mx-auto mb-6" />
+  </NuxtLink>
+  <div class="w-2/3 sm:w-1/2 pb-4 mx-auto">
+    <!-- Tab Buttons -->
+    <div
+      class="w-full flex bg-background mb-6 rounded-full border border-text p-1"
+    >
+      <button
+        @click="authMode = 'register'"
+        :class="[
+          'flex-1 py-2 text-center rounded-full',
+          authMode === 'register' ? 'bg-button text-white' : 'text-text',
+        ]"
+      >
+        Sign Up
+      </button>
+      <button
+        @click="authMode = 'login'"
+        :class="[
+          'flex-1 py-2 text-center rounded-full',
+          authMode === 'login' ? 'bg-button text-white' : 'text-text',
+        ]"
+      >
+        Sign In
+      </button>
     </div>
+
+    <!-- Login Form -->
+    <form
+      v-if="authMode === 'login'"
+      @submit.prevent="submitLoginForm"
+      class="w-full flex pb-16 flex-col gap-2 relative"
+    >
+      <TextField name="identifier" label="Email or Username" required />
+      <TextField name="password" label="Password" type="password" required />
+      <p
+        class="underline text-text text-right absolute top-18 right-0 cursor-pointer"
+        @click="forgetPassowrd"
+      >
+        Forget Password?
+      </p>
+      <p
+        v-show="loginErrorMessage"
+        class="mt-4 border border-cranberry-400 rounded p-2 bg-cranberry-100 text-center text-sm text-cranberry-400"
+      >
+        {{ loginErrorMessage }}
+      </p>
+      <TheButton :disabled="isLoginSubmitting" type="submit" class="w-full">
+        {{ isLoginSubmitting ? "Logging in..." : "Login" }}
+        <img
+          src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAy0lEQVR4nM3TUUqCURAF4J+MelFXILmKENTA1Si1CteQuQJRtxDtxUpsEb59MXTJ+Pv/bqAPnrfLmTn3cGamKE4N3GGJLfb4wBrDXGMDc7zhAV1coYMxXhN/WSfwhBe0a/gmnkOkiuxhE0UZl63kcJBJ4gBMS+8JVsV/4QvfIriJYKuKchil2uuYzjEOutgdk8F97Em56DbNOTeFNt7RryJnaQ9afzQHP6tTv8BjaRMjsEg9bMfP8Un1Jv4QGmCRGuIWduk2fts+C3wCojcuU/AEC20AAAAASUVORK5CYII="
+          alt="login-rounded-right"
+        />
+      </TheButton>
+    </form>
+
+    <!-- Register Form -->
+    <form
+      v-else
+      @submit.prevent="submitRegisterForm"
+      class="w-full flex flex-col gap-2"
+    >
+      <TextField name="email" label="Email" required />
+      <TextField name="username" label="Name" required />
+      <TextField name="password" label="Password" type="password" required />
+      <TextField
+        name="confirmPassword"
+        label="Confirm Password"
+        type="password"
+        required
+      />
+      <p
+        v-show="registerErrorMessage"
+        class="mt-4 border border-cranberry-400 rounded p-2 bg-cranberry-100 text-center text-sm text-cranberry-400"
+      >
+        {{ registerErrorMessage }}
+      </p>
+      <TheButton :disabled="isRegisterSubmitting" type="submit" class="w-full">
+        {{ isRegisterSubmitting ? "Creating account..." : "Create account" }}
+        <img
+          src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAy0lEQVR4nM3TUUqCURAF4J+MelFXILmKENTA1Si1CteQuQJRtxDtxUpsEb59MXTJ+Pv/bqAPnrfLmTn3cGamKE4N3GGJLfb4wBrDXGMDc7zhAV1coYMxXhN/WSfwhBe0a/gmnkOkiuxhE0UZl63kcJBJ4gBMS+8JVsV/4QvfIriJYKuKchil2uuYzjEOutgdk8F97Em56DbNOTeFNt7RryJnaQ9afzQHP6tTv8BjaRMjsEg9bMfP8Un1Jv4QGmCRGuIWduk2fts+C3wCojcuU/AEC20AAAAASUVORK5CYII="
+          alt="login-rounded-right"
+        />
+      </TheButton>
+    </form>
+    <RegisterationPopUp @close="closePopUP" v-if="openPopup" />
   </div>
 </template>
